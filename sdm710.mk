95c276b256c34cef3ff3c5cb2c340266876ea78a
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    sdm710-common: Adding Audio HAL V5: Direction API
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -30,9 +30,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     libqcompostprocbundle \
