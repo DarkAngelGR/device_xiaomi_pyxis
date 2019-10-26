3a0faccbaf3538c30278c37423baf48921bde3c3
argraur <argraur@gmail.com>
2019-10-26 17:08:09 +0000

    pyxis: Build vendor variant of libdng_sdk for watermark
    10-26 17:49:35.063  6856  6856 E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:911 LibMap() dlopen: dlopen failed: library "libdng_sdk.so" not found, on '/vendor/lib/camera/components/com.qti.node.watermark.so'
    
    Signed-off-by: argraur <argraur@gmail.com>
    Change-Id: Id5d4a73d29418bd3b7c0c9f92e4d5efd8be68c3b

--- device.mk
+++ device.mk
@@ -144,6 +144,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
+    libdng_sdk.vendor \
     Snap
 
 # Codec2 modules
