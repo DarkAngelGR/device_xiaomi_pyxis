b15f11c35ec4bc8d99432ec057f73c156259990b
Nanda Oktavera <codeharuka.yusa@gmail.com>
2019-11-20 09:21:07 +0900

    sdm710-common: compile more audio packages
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -31,10 +31,13 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.common@2.0-util \
+    android.hardware.audio.common@5.0-util \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
+    audio.r_submix.default \
+    audio.usb.default \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
