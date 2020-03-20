73f4efd84b66d9e2bcd783009028fddf46dd6a4b
coderzstas <coderz35@gmail.com>
2020-03-20 20:08:26 +0300

    moar cleanup

--- device.mk
+++ device.mk
@@ -379,16 +379,15 @@ PRODUCT_COPY_FILES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    ims-ext-common \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext \
-    ims-ext-common_system
+    telephony-ext
 
 PRODUCT_BOOT_JARS += \
-    telephony-ext \
-    ims-ext-common_system
+    telephony-ext
 
 # Thermal
 PRODUCT_PACKAGES += \
