9dee63a3117a2ff7d4c43fe4af872439fc4d6a9d
coderzstas <coderz35@gmail.com>
2020-03-30 13:44:16 +0300

    pyxis : build ble hearing aid

--- device.mk
+++ device.mk
@@ -221,6 +221,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
+    audio.hearing_aid.default \
     audio.primary.sdm710 \
     audio.r_submix.default \
     audio.usb.default \
