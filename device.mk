a749eb3166bed36da878517dfe46d4354816dbfb
DarkAngelGR <asavvo01@gmail.com>
2020-05-07 17:43:53 +0000

    pyxis: Add DerpFest overlays
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- device.mk
+++ device.mk
@@ -31,7 +31,8 @@ EXTRA_FOD_ANIMATIONS := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-derpfest
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
