77cd48b883e4a4dc4abf858aca69ef8ba9fbcb63
DarkAngelGR <asavvo01@gmail.com>
2020-05-12 11:50:47 +0000

    pyxis: Actually build GAPPS
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- derp_pyxis.mk
+++ derp_pyxis.mk
@@ -7,13 +7,10 @@
 # Inherit some common Derp stuff.
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-BUILD_GAPPS := true
-ifeq (($(BUILD_GAPPS), true)
 IS_PHONE := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_WIFI_EXT := true
-endif
 
 # Inherit from pyxis device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
