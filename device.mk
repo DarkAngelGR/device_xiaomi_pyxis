dc365d490d424e8f36b940411870f4924d15cbea
DarkAngelGR <asavvo01@gmail.com>
2020-05-13 09:13:39 +0000

    pyxis: Don't build Atrace hal
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- device.mk
+++ device.mk
@@ -411,10 +411,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Tracing
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service.pixel
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
